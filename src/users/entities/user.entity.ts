f9d26a98b217d3b9a76efc8f0015e8ae98f05fe9
olliso17 <olli.so17@gmail.com>
2024-01-22 07:26:19 -0300

    feat(all): update schema and entities

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,17 @@
 import { Login } from 'src/logins/entities/login.entity';
 import { Ong } from 'src/ongs/entities/ong.entity';
+import { User } from '@prisma/client';
 
-export class User {
+export class UserEntity implements User {
   id: string;
   name: string;
   email: string;
   password: string;
   ongs?: Ong[];
   active: boolean;
-  image?: string;
+  image: string | null;
   created_at: Date;
   updated_at: Date;
-  deleted_at?: Date | null;
+  deleted_at: Date | null;
   logins?: Login[];
 }
