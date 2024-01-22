f8bb9a2d0b068719202bc32cf191c3eef02287c2
olliso17 <olli.so17@gmail.com>
2024-01-22 06:12:37 -0300

    feat(entities): update entity user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Login } from 'src/logins/entities/login.entity';
+import { Ong } from 'src/ongs/entities/ong.entity';
+
+export class User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  ongs?: Ong[];
+  active: boolean;
+  image?: string;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at?: Date | null;
+  logins?: Login[];
+}
