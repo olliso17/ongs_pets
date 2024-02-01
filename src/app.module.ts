efab2ec85017a374afd340620212bb6b78619527
patricia <patriciasilva>
2024-02-01 12:14:04 +0000

    feat(all): update all

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { LoginsModule } from './logins/logins.module';
-import { DonationsModule } from './donations/donations.module';
-import { PetsModule } from './pets/pets.module';
-import { OngsModule } from './ongs/ongs.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import dataSource, { dataSourceOptions } from 'db/data-source';
-import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { LoginsModule } from "./logins/logins.module";
+import { DonationsModule } from "./donations/donations.module";
+import { PetsModule } from "./pets/pets.module";
+import { OngsModule } from "./ongs/ongs.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import dataSource, { dataSourceOptions } from "db/data-source";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
-    UsersModule, OngsModule, PetsModule, DonationsModule, LoginsModule, RabbitmqModule],
+    UsersModule,
+    OngsModule,
+    PetsModule,
+    DonationsModule,
+    LoginsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,20 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { CreateUserInputDto, CreateUserOutputDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import User from './entities/user.entity';
-import { Repository } from 'typeorm';
-import { UserRepository } from './user.repository';
-import { MinCountCaractersPassword, StringNotNullAndBlankSpace } from 'src/util/verify.regex';
+import { Inject, Injectable } from "@nestjs/common";
+import { CreateUserInputDto, CreateUserOutputDto } from "./dto/create-user.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import User from "./entities/user.entity";
+import { Repository } from "typeorm";
+import { UserRepository } from "./user.repository";
+import {
+  MinCountCaractersPassword,
+  StringNotNullAndBlankSpace,
+} from "src/util/verify.regex";
 const bcrypt = require("bcryptjs");
 @Injectable()
 export class UsersService {
   constructor(
     // @Inject("UserRepo")
     private usersRepository: UserRepository,
-
   ) {}
-
 
   findAll() {
     return `This action returns all users`;
@@ -24,22 +24,19 @@ export class UsersService {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
 
   remove(id: number) {
     return `This action removes a #${id} user`;
   }
 
-  encryptValue(
-    value: string,
-    type:string
-  ) {
+  encryptValue(value: string, type: string) {
     if (StringNotNullAndBlankSpace.test(value) === false) {
-      throw new Error(type+"is required.");
+      throw new Error(type + "is required.");
     }
-    if(type === "Password" && MinCountCaractersPassword.test(value)){
+    if (type === "Password" && MinCountCaractersPassword.test(value)) {
       throw new Error("Password must be at least 4 characters long.");
     }
     const salt = bcrypt.genSaltSync(10);
