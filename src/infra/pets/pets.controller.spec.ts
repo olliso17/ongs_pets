262b9da6cb7c80b5cab506d54b65d273549b0942
patricia <patriciasilva>
2024-02-07 12:11:49 +0000

    feat(test): update test controller

--- src/infra/pets/pets.controller.spec.ts
+++ src/infra/pets/pets.controller.spec.ts
@@ -1,19 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PetsController } from './pets.controller';
 import CreatePetUsecase from '../../usecases/pets/create.pet.usecase';
+import { PetRepository } from './pets.repository';
+import FindPetByIdUsecase from '../../usecases/pets/find.by.pet.id';
+import FindAllActivePetsUsecase from '../../usecases/pets/find.all.active.pet.usecase';
+import ActivatePetUsecase from '../../usecases/pets/activate.pet.usecase';
+import { EditPetUsecase } from '../../usecases/pets/edit.pet.usecase';
 
 describe('PetsController', () => {
   let controller: PetsController;
+  let petRepository: PetRepository;
+  let createPetUsecase: CreatePetUsecase;
+  let findAllActivePetsUsecase: FindAllActivePetsUsecase;
+  let findPetByIdUsecase: FindPetByIdUsecase;
+  let activatePetUsecase: ActivatePetUsecase;
+  let editPetUsecase: EditPetUsecase;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [PetsController],
-      providers: [CreatePetUsecase],
-    }).compile();
-
-    controller = module.get<PetsController>(PetsController);
+  beforeEach(() => {
+    const typeOrmMock: any = {};
+    petRepository = new PetRepository(typeOrmMock);
+    createPetUsecase = new CreatePetUsecase(petRepository);
+    findPetByIdUsecase = new FindPetByIdUsecase(petRepository);
+    findAllActivePetsUsecase = new FindAllActivePetsUsecase(petRepository);
+    controller = new PetsController(createPetUsecase, findPetByIdUsecase, findAllActivePetsUsecase, editPetUsecase, activatePetUsecase)
   });
-
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
